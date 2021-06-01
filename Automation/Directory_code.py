3e84e404cf47e9d61383b30c3e2b94e41369628a
VISHAL HASE <vishalhase1111@gmail.com>
2021-06-01 17:38:14 +0530

    unit testing sample added

--- Automation/Directory_code.py
+++ Automation/Directory_code.py
@@ -2,11 +2,11 @@
 The create_python_script function creates a new python script in the current working directory, adds the line of comments to it declared by the 'comments' variable, and returns the size of the new file. Fill in the gaps to create a script called "program.py"."""
 
 comments = "# Start of a new Python program"
-with open(filename, 'w') as file:
-    filesize = file.write(comments)
-return (filesize)
+#with open(filename, 'w') as file:
+#    filesize = file.write(comments)
+#return (filesize)
 
-print(create_python_script("program.py"))
+#print(create_python_script("program.py"))
 
 
 
